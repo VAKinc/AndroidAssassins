1b94a0d65b8cf8d8f4ef73c5543dbf4dbb295005
VAKinc <nolan.josephjp@gmail.com>
2019-10-04 10:44:56 +0900

    Update Constants.php

--- htdocs/assassins/includes/Constants.php
+++ htdocs/assassins/includes/Constants.php
@@ -1,7 +1,7 @@
 <?php 
-	define('DB_HOST', '192.168.3.14');
-	define('DB_PORT', '3306');
-	define('DB_USER', 'client');
-	define('DB_PASS', 'ybPKYVl9fsO8MkAg');
+	define('DB_HOST', 'YOUR IP HERE');
+	define('DB_PORT', '3306'); //This may need to be changed depending on your setup, but should work by default.
+	define('DB_USER', 'YOUR USER NAME HERE');
+	define('DB_PASS', 'YOUR PASSWORD HERE');
     define('DB_NAME', 'assassins');
 ?>
